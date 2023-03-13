9d24a773ecfda07fed5b3699be485662dd62c3e5
layolabernat <127501947+layolabernat@users.noreply.github.com>
2023-03-13 14:24:35 +0100

    Joc.tcsn

--- Personatge.gd
+++ Personatge.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var velocitat := 500
 var moviment := Vector2.ZERO
 var gravetat := Vector2.DOWN * 350
-var salt := Vector2.UP * 275
+var salt := Vector2.UP * 325
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
